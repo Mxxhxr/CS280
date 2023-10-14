c412449f13a8b62bed27fc77f84aee5e89141c86
Maahir Vohra <117885945+Mxxhxr@users.noreply.github.com>
2023-10-14 19:49:51 -0400

    updates

---
+++ PA1/lex.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <sstream>
+#include <map>
+#include <cctype>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+
+LexItem getNextToken(instream& in, int& linenumber){
+
+}
+
+int main(int argc, char *argv[]) { 
+    
+}
--- Week4/mapTest.cpp
+++ Week4/mapTest.cpp
@@ -7,23 +7,28 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
 
-	map<string, int> myMap;
+	// map<string, int> myMap;
 	
-	myMap["maahir"] = 20;
-	myMap["masoom"] = 23;
-	myMap["zaid"] = 21;
+	// myMap["maahir"] = 20;
+	// myMap["masoom"] = 23;
+	// myMap["zaid"] = 21;
 
 
-	map<string, int>::iterator it;
+	// map<string, int>::iterator it;
 
 
 	
-	for(auto it = myMap.begin(); it != myMap.end(); it++) {
-		cout << "Name: " << it->first << ": Age: " << it->second << endl;
-	}
+	// for(auto it = myMap.begin(); it != myMap.end(); it++) {
+	// 	cout << "Name: " << it->first << ": Age: " << it->second << endl;
+	// }
 
-	myMap.insert(make_pair("zainab", 17));
+	// myMap.insert(make_pair("zainab", 17));
 
-	cout << myMap["zainab"];
+	// cout << myMap["zainab"];
+
+	int i = 5;
+	const int * p = &i;
+	int result = *p + 5;
+	cout << result << endl;
 }
 
