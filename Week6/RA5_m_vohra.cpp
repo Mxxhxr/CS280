74add9bebe1546e1c726026fd3d07856d163bd0f
Maahir Vohra <Maahir Vohra>
2023-10-15 13:40:09 -0400

    ra5

--- Week6/RA5_m_vohra.cpp
+++ Week6/RA5_m_vohra.cpp
@@ -3,7 +3,72 @@
 
 using namespace std;
 
-LexItem id_or_kw(const string& lexeme, int linenum) {
-    
-}
+LexItem id_or_kw(const string &lexeme, int linenum)
+{
+    map<string, Token> lexToTok = {
+        {"if", IF},
+        {"else", ELSE},
+        {"writeln", WRITELN},
+        {"write", WRITE},
+        {"integer", INTEGER},
+        {"real", REAL},
+        {"boolean", BOOLEAN},
+        {"string", STRING},
+        {"begin", BEGIN},
+        {"end", END},
+        {"var", VAR},
+        {"then", THEN},
+        {"program", PROGRAM},
 
+        {"true", BCONST},
+        {"false", BCONST},
+
+        {"+", PLUS},
+        {"-", MINUS},
+        {"*", MULT},
+        {"/", DIV},
+        {"div", IDIV},
+        {"mod", MOD},
+        {":=", ASSOP},
+        {"=", EQ},
+        {">", GTHAN},
+        {"<", LTHAN},
+        {"and", AND},
+        {"or", OR},
+        {"not", NOT},
+
+        {",", COMMA},
+        {";", SEMICOL},
+        {"(", LPAREN},
+        {")", RPAREN},
+        {".", DOT},
+        {":", COLON}
+
+        //{"done", DONE}
+        //{"err", ERR}
+        // {"rconst", RCONST},
+        // {"sconst", SCONST},
+        //{"iconst", ICONST}};
+        //{"ident", PROGRAM}, if doesnt match, assign to IDENT
+    };
+
+    for (auto const &x : lexToTok)
+    {
+        cout << x.first << endl;
+        cout << x.second << endl;
+    }
+}
+// switch (lexeme)
+// {
+// case ";":
+//     return LexItem(SEMICOL, lexeme, lineum);
+// }
+
+// create a map that has lexeme, token
+// search map for lexeme and create LexItem with the corresponding token
+// pass it to operator<<() function and print out Lexitem object according to token value
+
+int main()
+{
+    id_or_kw(".", 3);
+}
--- Week6/tokensListing.cpp
+++ Week6/tokensListing.cpp
@@ -7,22 +7,22 @@
 
 /*
  * CS280 - Fall 2023
-*/
-
+ */
 
 using std::map;
 
 using namespace std;
 
-istream& operator>>(istream& in, LexItem& tok) {
-	
-	map <string, Token> StrToTok = {
+istream &operator>>(istream &in, LexItem &tok)
+{
+
+	map<string, Token> StrToTok = {
 		{"PROGRAM", PROGRAM},
 		{"WRITELN", WRITELN},
-		{"WRITE", WRITE },
-		{ "IF", IF },
-		{ "ELSE", ELSE },	
-		{ "IDENT", IDENT },
+		{"WRITE", WRITE},
+		{"IF", IF},
+		{"ELSE", ELSE},
+		{"IDENT", IDENT},
 		{"VAR", VAR},
 		{"BEGIN", BEGIN},
 		{"END", END},
@@ -32,76 +32,75 @@ istream& operator>>(istream& in, LexItem& tok) {
 		{"BOOLEAN", BOOLEAN},
 		{"TRUE", TRUE},
 		{"FALSE", FALSE},
-		
-		{ "ICONST", ICONST },
-		{ "RCONST", RCONST },
-		{ "SCONST", SCONST  },
-		{ "BCONST", BCONST },
-		
-		{ "PLUS", PLUS },
-		{ "MINUS" , MINUS },
-		{ "MULT" , MULT  },
-		{ "DIV" , DIV },
-		{ "IDIV" , IDIV },
-		{ "ASSOP", ASSOP },
-		{ "EQ", EQ  },
-		{ "GTHAN" , GTHAN  },
-		{ "LTHAN", LTHAN },
-		{ "MOD", MOD},
-		{ "AND", AND},
-		{ "OR", OR},
-		{ "NOT", NOT},
 
-		{ "COMMA", COMMA  },
-		{ "LPAREN", LPAREN },
-		{ "RPAREN", RPAREN },
-		
-		{ "SEMICOL", SEMICOL  },
-		{ "DOT", DOT },
-		
-		{ "ERR",ERR  },
+		{"ICONST", ICONST},
+		{"RCONST", RCONST},
+		{"SCONST", SCONST},
+		{"BCONST", BCONST},
 
-		{ "DONE", DONE },
-};
+		{"PLUS", PLUS},
+		{"MINUS", MINUS},
+		{"MULT", MULT},
+		{"DIV", DIV},
+		{"IDIV", IDIV},
+		{"ASSOP", ASSOP},
+		{"EQ", EQ},
+		{"GTHAN", GTHAN},
+		{"LTHAN", LTHAN},
+		{"MOD", MOD},
+		{"AND", AND},
+		{"OR", OR},
+		{"NOT", NOT},
+
+		{"COMMA", COMMA},
+		{"LPAREN", LPAREN},
+		{"RPAREN", RPAREN},
+
+		{"SEMICOL", SEMICOL},
+		{"DOT", DOT},
+
+		{"ERR", ERR},
+
+		{"DONE", DONE},
+	};
 	Token tt;
 	string tokstr, lexeme, str2;
 	int line, strlen, i;
 	char ch;
-		
-	in >> tokstr ;
-	if(!in)
+
+	in >> tokstr;
+	if (!in)
 	{
 		return in;
 	}
 	auto ttitr = StrToTok.find(tokstr);
-	//cout << tokstr << endl;
-	if(ttitr != StrToTok.end())
+	// cout << tokstr << endl;
+	if (ttitr != StrToTok.end())
 	{
 		tt = ttitr->second;
-		if(tt == IDENT)
+		if (tt == IDENT)
 		{
 			in >> lexeme >> line;
 			tok = id_or_kw(lexeme, line);
-		
 		}
-		else if (tt == ICONST || tt == RCONST )
+		else if (tt == ICONST || tt == RCONST)
 		{
 			in >> lexeme >> line;
 			LexItem item(tt, lexeme, line);
 			tok = item;
-		
 		}
 		else if (tt == SCONST)
 		{
 			in >> strlen;
-			str2 = ""; i = 0;
+			str2 = "";
+			i = 0;
 			ch = in.get();
 			while (ch != '\"')
 			{
 				ch = in.get();
 			}
 			ch = in.get();
-			while ( (ch != '\"') && (i < strlen))
+			while ((ch != '\"') && (i < strlen))
 			{
 				str2 += ch;
 				ch = in.get();
@@ -111,28 +110,26 @@ istream& operator>>(istream& in, LexItem& tok) {
 			LexItem item(tt, str2, line);
 			tok = item;
 		}
-		else 
+		else
 		{
 			in >> lexeme >> line;
 			LexItem item(tt, lexeme, line);
 			tok = item;
-			
 		}
-		
 	}
-	
+
 	return in;
 }
 
 int main(int argc, char *argv[])
 {
 	LexItem item(ERR, "", 0);
-	LexItem& tokItem = item;
+	LexItem &tokItem = item;
 	LexItem kwtok;
-	ifstream  file ;
+	ifstream file;
 
 	string str1;
-	
+
 	if (argc == 1)
 	{
 		cerr << "NO SPECIFIED INPUT FILE." << endl;
@@ -140,22 +137,22 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		if(argv[1][0] == '-')
+		if (argv[1][0] == '-')
 		{
 			cerr << "NO SPECIFIED INPUT FILE." << endl;
 			return 0;
 		}
 		file.open(argv[1]);
-		if( file.is_open() == false ) {
+		if (file.is_open() == false)
+		{
 			cerr << "CANNOT OPEN THE FILE " << argv[1] << endl;
 			return 0;
 		}
-
 	}
 	while (file >> tokItem)
 	{
 		cout << tokItem; //<< endl;
 	}
 
-    return 0;
+	return 0;
 }
