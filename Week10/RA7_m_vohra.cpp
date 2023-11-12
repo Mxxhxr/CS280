839d4a7b35db6f275f6232915367800c87357acc
Maahir <maahir.vohra14@gmail.com>
2023-11-12 14:24:30 -0500

    test

---
+++ Week10/RA7_m_vohra.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <queue>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+bool inLanguage(string & inputString) {
+
+    if(inputString.empty()) {
+        cout << "string is missing #." << endl;
+        cout << "\"" << inputString << "\" " << "is not a sentence in the Language." << endl;
+
+    }
+}
