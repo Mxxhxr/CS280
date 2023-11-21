6c7b4ef017c16f1793b9d70ca5181f975131f465
Maahir Vohra <117885945+Mxxhxr@users.noreply.github.com>
2023-11-20 21:11:10 -0500

    ra8

--- Week11/val.cpp
+++ Week11/val.cpp
@@ -11,30 +11,29 @@ Value Value::operator/(const Value& op) const {
 
 
 //overloaded equality operator of this with op
-Value Value::operator==(const Value& op) const {
-    if (this->GetType() == VERR || op.GetType() == VERR) {
-        // If either operand is an error, consider them equal
-        return Value(false);
-    }
-
-    if (this->GetType() == op.GetType()) {
-        // Compare values based on their types
-        switch (this->GetType()) {
-            case VINT:
-                return Value(this->GetInt() == op.GetInt());
-            case VREAL:
-                return Value(this->GetReal() == op.GetReal());
-            case VSTRING:
-                return Value(this->GetString() == op.GetString());
-            case VBOOL:
-                return Value(this->GetBool() == op.GetBool());
-            default:
-                throw std::logic_error("Comparison not supported for this type");
+Value Value::operator==(const Value &op) const
+{
+    if (GetType() == op.GetType()) {
+        // same type
+        if (IsInt() && op.IsInt()) {
+            return Value(Itemp == op.GetInt());
+        }
+        else if (IsReal() && op.IsReal()) {
+            return Value(Rtemp == op.GetReal());
+        }
+        else if (IsString() && op.IsString()) {
+            return Value(Stemp == op.GetString());
         }
     }
-
-    // If types are different, return false
-    return Value(false);
+    else {
+        if (IsInt() && op.IsReal()) {
+            return Value(false);
+        }
+        else if (IsReal() && op.IsInt()) {
+            return Value(false);
+        }
+    }
+    return Value();
 }
 
 
