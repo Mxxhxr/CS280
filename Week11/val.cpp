84a0daf23106dd8f5985ea7fdde07332d54f2597
Maahir <maahir.vohra14@gmail.com>
2023-11-20 22:24:28 -0500

    RA8 5/5

--- Week11/val.cpp
+++ Week11/val.cpp
@@ -3,12 +3,20 @@
 
 // arithmetic overloaded divide this by op
 Value Value::operator/(const Value& op) const {
-    if(this->GetType() != op.GetType()) {
-        return Value(false);
+    if (GetType() == VINT && op.GetType() == VINT) {
+        return Value(Itemp / op.GetInt());
     }
-    return Value(true);
+    else if (GetType() == VREAL && op.GetType() == VREAL) {
+        return Value(Rtemp / op.GetReal());
+    }
+    else if(GetType() == VREAL && op.GetType() == VINT) {
+        return Value(Rtemp / op.GetInt());
+    }
+    else if(GetType() == VINT && op.GetType() == VREAL) {
+        return Value(Itemp / op.GetReal());
+    }
+    return Value();
 }
-
 
 //overloaded equality operator of this with op
 Value Value::operator==(const Value &op) const
@@ -24,6 +32,9 @@ Value Value::operator==(const Value &op) const
         else if (IsString() && op.IsString()) {
             return Value(Stemp == op.GetString());
         }
+        else if(IsBool() && op.IsBool()) {
+            return Value(Btemp == op.GetBool());
+        }
     }
     else {
         if (IsInt() && op.IsReal()) {
@@ -36,21 +47,40 @@ Value Value::operator==(const Value &op) const
     return Value();
 }
 
-
-
 //Overloaded Anding operator
 Value Value::operator&&(const Value& oper) const {
-    if(this->GetType() != oper.GetType()) {
-        return Value(false);
+    if(GetType() != VBOOL) {
+        return Value();
     }
-    return Value(true);
+    else if(GetType() == VBOOL && oper.GetType() == VERR) {
+        return Value();
+    }
+    else if(GetType() == VERR && oper.GetType() == VBOOL) {
+        return Value();
+    }
+    else if(GetType() == VBOOL && oper.GetType() == VINT) {
+        return Value();
+    }
+    else if(GetType() == VBOOL && oper.GetType() == VREAL) {
+        return Value();
+    }
+    else if(GetType() == VBOOL && oper.GetType() == VSTRING) {
+        return Value();
+    }
+    else {
+        return Value(Btemp == oper.GetBool());
+    }
+    return Value();
 }
-
 
 // arithmetic overloaded modulus this by op
 Value Value::operator%(const Value& oper) const {
-    if(this->GetType() != oper.GetType()) {
-        return Value(false);
+    if (GetType() == VINT && oper.GetType() == VINT) {
+        // int mod
+        return Value(Itemp % oper.GetInt());
+    } 
+    else {
+        return Value();
     }
-    return Value(true);
-}   
+    return Value();
+}
